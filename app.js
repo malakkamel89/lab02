7730aed82fb521f8df06f75e8b8d9dec341ceb0f
malakkamel89 <>
2021-06-16 15:34:22 +0000

    make changes

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("welcome to jordan")
+
+var customerAge = prompt("Please enter your age", "");
+
+var customerAge = "18";
+if (customerAge < 18) { type
+   "Good day";
+} else { type
+  = "Good evening";
+}
+
+
+
+
+
+
