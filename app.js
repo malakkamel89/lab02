251c566a2398773103b105fc2eabb04e8db07c00
malakkamel89 <>
2021-06-17 13:44:53 +0000

    new question

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ alert("welcome to jordan")
 var customerAge = prompt("Please enter your age");
 customerAge = parseInt(customerAge)
 
-if (customerAge < 18) {
-  alert("Good day");
-} else {
-  alert("Good evening")
+
+while (customerAge < 18) {
+  alert("your age under 18 please enter yourage again")
+  customerAge = prompt("Please enter your age");
+  customerAge = parseInt(customerAge)
 }
 
+var numofvisits = 2;
+numofvisits = prompt("Please number of visits to Jordan");
+numofvisits = parseInt(customerAge)
+
+var i;
+for (i = 0; i <numofvisits ; i++) {document.write("<img src='jordan5.jpg' >")}
+
+  
 
 
 
