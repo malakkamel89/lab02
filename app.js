69c2e41713302125fef5322c8cb4b51645c58653
malakkamel89 <>
2021-06-20 11:30:23 +0000

    newlab

--- app.js
+++ app.js
@@ -3,11 +3,47 @@ alert("welcome to jordan")
 var customerAge = prompt("Please enter your age");
 customerAge = parseInt(customerAge)
 
-if (customerAge < 18) {
-  alert("Good day");
-} else {
-  alert("Good evening")
+
+while (customerAge < 18) {
+  alert("your age under 18 please enter yourage again")
+  customerAge = prompt("Please enter your age");
+  customerAge = parseInt(customerAge)
 }
+
+var numofvisits = 2;
+numofvisits = prompt("Please number of visits to Jordan");
+numofvisits = parseInt(customerAge)
+
+var i;
+for (i = 0; i <numofvisits ; i++) {document.write("<img src='jordan5.jpg' >")}
+
+
+
+function visits() {
+  var visits = prompt( "how many times did visit Joradn?")
+  return parseInt(visits)
+
+}
+
+function rating(){
+  varstarsNum= prompt("how many starts do you rate our website?")
+  return parseInt(starsNum);
+  
+
+  }
+var rate= rating()
+for(var i=0;i<rate;i++){
+    document.write("<img) src= 'star.jpg' >")}
+
+
+
+
+
+  
+
+
+
+
 
 
 
